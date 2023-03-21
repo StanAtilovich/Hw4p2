92419ef5de5883918e4cc4a8877821a6be81d661
StanAtilovich <alano92@yandex.ru>
2023-03-21 11:35:35 +0300

    Start next hw

--- app/src/main/java/com/example/hw4/repository/PostRepository.kt
+++ app/src/main/java/com/example/hw4/repository/PostRepository.kt
@@ -5,15 +5,26 @@ import com.example.hw4.DTO.Post
 
 
 interface PostRepository {
-    fun getAll(callBack: PostCallBack)
-    fun likedById(id: Long):Post
-    fun unlikedById(id: Long):Post
-    fun sharing(id: Long)
-    fun removeById(id: Long)
-    fun save(post: Post)
+    fun likedByIdAsync(id: Long,likedByMe:Boolean,callback: Callback<Post>)
+  //  fun unlikedByIdAsync(id: Long, callback: PostCallBack<Post>)
+    fun saveAsync(post: Post, callback: SaveRemoveCallback)
+    fun removeById(id: Long,callback: SaveRemoveCallback)
+    fun getAllAsync(callback: PostCallBack<List<Post>>)
 
-    interface PostCallBack {
-        fun onSuccess(post: List<Post>)
-        fun onError(e: java.lang.Exception)
+    interface PostCallBack<T> {
+        fun onSuccess(value: T){}
+        fun onError(e: java.lang.Exception){}
     }
+
+    interface SaveRemoveCallback {
+        fun onSuccess() {}
+        fun onError(e: Exception) {}
+    }
+
+    interface Callback<T> {
+        fun onSuccess(posts: T) {}
+        fun onError(e: Exception) {}
+    }
+
+
 }
